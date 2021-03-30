7b0a14f542e4e668351166f8e4db04fecd19f4ed
ac101m <mofty101@gmail.com>
2021-03-30 13:03:04 +0100

    Remove execution privileges from python files

--- rm_colour.py
+++ rm_colour.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 
 import curses
 
@@ -7,4 +5,3 @@ import curses
 # "Static" class containing colors for various elements
 # Use this as a sort of settings menu for program appearance
 def resmon_color:
-
